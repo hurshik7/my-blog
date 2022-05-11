7a273f10f4be7b8db6f87bef3ffd105cf2389053
hurshik7 <hurshik7@gmail.com>
2022-05-11 15:49:20 -0700

    implement template

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
 
-  let post = "Hello World";
+  let [titles, setTitles] = useState(["Hello World1", "HelloWorld2", "HelloWorld3"]);
+  let [likeCount, setLikeCount] = useState(0);
 
   return (
     <div className="App">
       <div className="black-nav">
         <h4>Blogit</h4>
       </div>
-      <h4>{post}</h4>
+      <div className="list">
+        <h4>{titles[0]}  <span onClick={() => {setLikeCount(likeCount + 1)}}>👍</span> {likeCount}</h4>
+        <p>May 11, 2022</p>
+      </div>
+      <div className="list">
+        <h4>{titles[1]}</h4>
+        <p>May 12, 2022</p>
+      </div>
+      <div className="list">
+        <h4>{titles[2]}</h4>
+        <p>May 13, 2022</p>
+      </div>
     </div>
   );
 }
