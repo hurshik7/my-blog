015b34740c43a82f8c3d3b8b7393277d5a9b2136
hurshik7 <hurshik7@gmail.com>
2022-05-12 10:47:08 -0700

    implement add a post

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import './App.css';
 import { useState } from 'react';
 
-function Modal() {
-  return (
-    <div className="modal">
-        <h4>Title</h4>
-        <p>Date</p>
-        <p>Contents</p>
-    </div>
-  );
-}
-
 function App() {
 
-  let [posts, setPosts] = useState([{"title": "d", "likes": 0},
-                                    {"title": "a", "likes": 0},
-                                    {"title": "G", "likes": 0}]);
+  let [posts, setPosts] = useState([{"title": "Hello world", "likes": 0},
+                                    {"title": "My name is Shik", "likes": 0},
+                                    {"title": "What's going on stock market now", "likes": 0}]);
   let [modal, setModal] = useState(false);
+  let [clickedIndex, setClikedIndex] = useState(-1);
+  let [input, setInput] = useState('');
 
   return (
     <div className="App">
@@ -34,20 +26,52 @@ function App() {
             <div className="list">
               <h4 onClick={() => {
                 setModal(!modal);
-              }}>{posts[i].title}  <span onClick={() => {
+                setClikedIndex(i);
+              }}>{posts[i].title}  <span onClick={(e) => {
+                e.stopPropagation();
                 let postsCopied = [...posts];
                 postsCopied[i].likes = postsCopied[i].likes + 1;
                 setPosts(postsCopied);
               }}>👍</span> {element.likes}</h4>
               <p>May 11, 2022</p>
+              <button onClick={() => {
+                
+              }}>Delete</button>
             </div>
           );
         })
       }
 
+      <input onChange={(e) => {
+        setInput(e.target.value);
+        console.log(input);
+      }}/>
+      <button onClick={() => {
+        let postsCopied = [...posts];
+        let newPost = {"title": input, "likes": 0};
+        postsCopied.push(newPost);
+        setPosts(postsCopied);
+      }}>submit!</button>
+
       {
-        modal ? <Modal /> : null
+        modal ? <Modal posts={posts} setPosts={setPosts} color="skyblue" index={clickedIndex}/> : null
       }
+    </div>
+  );
+}
+
+function Modal(props) {
+  return (
+    <div className="modal" style={{background : props.color}}>
+        <h4>{props.posts[props.index].title}</h4>
+        <p> 👍 {props.posts[props.index].likes}</p>
+        <p>Date</p>
+        <p>Contents</p>
+        <button onClick={() => {
+          let postsCopied = [...props.posts];
+          postsCopied[props.index].title = "Edited";
+          props.setPosts(postsCopied);
+        }}>Edit</button>
     </div>
   );
 }
